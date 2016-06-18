54e74879bee4dd1068d19f5fa6c03b83c4144dbc
Sophie Qiu <hqiu@cs.princeton.edu>
2016-06-18 15:07:28 -0400

    Day 1 morning madlib

---
+++ Day1/madlib.py
@@ -0,0 +1,25 @@
+a1 = raw_input("Give me an adjective: ")
+a2 = raw_input("Give me another adjective: ")
+a3 = raw_input("Give me another adjective: ")
+a4 = raw_input("Give me anohter adjective (the last one, I promise!): ")
+n1 = raw_input("Give me a noun: ")
+a5 = raw_input("Give me an adjective (oops): ")
+v1 = raw_input("Give me a verb ending in -ing: ")
+an1 = raw_input("Give ma a name of an animal: ")
+v2 = raw_input("Give me a verb ending in -ing: ")
+an2 = raw_input("Give me a name of an animal: ")
+n2 = raw_input("Name a food: ")
+n3 = raw_input("Name a liquid: ")
+v3 = raw_input("Give me a verb ending in -ing: ")
+n4 = raw_input("Think of a title for a romance novel: ")
+a6 = raw_input("Give me an adjective (THIS IS THE LAST ONE!): ")
+
+print("In my opinion, winter is the most " + a1 +
+      " time of year. Especially in Denver, where the air is " + a2 +
+      " and " + a3 + ". Since the climate is " + a4 + " and it's dry as a " +
+      n1 + " nothing sounds better than a " + a5 + " yoga class! Currently, " +
+      "I'm working on perfecting " + v1 + " " + an1 + ", but my all time favorite " +
+      "yoga pose is " + v2 + " " + an2 + ". Yoga is my favorite activity, but I " +
+      "also love to spend time cooking " + n2 + ", and I love to drink hot " + n3 +
+      ". Yum! I also enjoy " + v3 + "in front of the fire, and reading " + n4 +
+      " on a blustery, " + a6 + " afternnon. Life in Denver is fabulous!")
